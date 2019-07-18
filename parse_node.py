cd90372136e5253cca9a1880c196ba9177d33283
Alex <alex.hudson@iqgeo.com>
2019-07-18 16:45:20 +0100

    reset back to internet specs

--- parse_node.py
+++ parse_node.py
@@ -1,12 +1,38 @@
 
-class Lexer(object):
-    # Token types
-    tokens = {
+# Token types
+#
+# EOF (end-of-file) token is used to indicate that
+# there is no more input left for lexical analysis
+INTEGER, MUL, DIV, EOF = 'INTEGER', 'MUL', 'DIV', 'EOF'
 
-    }
+
+class Token(object):
+    def __init__(self, type, value):
+        # token type: INTEGER, MUL, DIV, or EOF
+        self.type = type
+        # token value: non-negative integer value, '*', '/', or None
+        self.value = value
+
+    def __str__(self):
+        """String representation of the class instance.
+
+        Examples:
+            Token(INTEGER, 3)
+            Token(MUL, '*')
+        """
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=repr(self.value)
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Lexer(object):
     def __init__(self, text):
         # client string input, e.g. "3 * 5", "12 / 3 * 4", etc
-        self.text = "12 / 3* 4"
+        self.text = "12 / 3 * 4"
         # self.pos is an index into self.text
         self.pos = 0
         self.current_char = self.text[self.pos]
